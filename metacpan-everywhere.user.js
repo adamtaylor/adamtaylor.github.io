96dff55fc131929dd6e7fb6957683490f590517e
Adam Taylor <adamjctaylor@gmail.com>
2011-08-02 00:07:55 +0100

    Update MetaCPAN greasemonkey script

--- metacpan-everywhere.user.js
+++ metacpan-everywhere.user.js
@@ -11,11 +11,11 @@
 
     var page_links = document.links;
     for (var i=0; i<page_links.length; i++){
-        var match_url = page_links[i].href.match(/http:\/\/search\.cpan\.org\/perldoc\?(.*?)$/i);
-        if (match_url) {
-            alert match_url[1];
+        if (page_links[i].href.match(/http:\/\/search\.cpan\.org\/perldoc\?(.*?)$/i)) {
+            var match = page_links[i].href.match(/http:\/\/search\.cpan\.org\/perldoc\?(.*?)$/i)
+            alert match[1];
             var span = document.createElement("span");
-            span.innerHTML = "<a href=\"http://www.metacpan.org/module/"+match_url[1]+"\">MetaCPAN</a>";
+            span.innerHTML = "<a href=\"http://www.metacpan.org/module/"+match[1]+"\">MetaCPAN</a>";
             page_links[i].parentNode.insertBefore(span, page_links[i].nextSibling)
         }
     }
